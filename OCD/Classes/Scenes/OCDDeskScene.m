5fe47fe425382aaffa21d2ba347d47468a1e2abb
Michael Gao <mgao.ucla@gmail.com>
2014-05-27 00:06:34 -0700

    Added stain, writing, and paper to scene

--- OCD/Classes/Scenes/OCDDeskScene.m
+++ OCD/Classes/Scenes/OCDDeskScene.m
@@ -7,6 +7,18 @@
 //
 
 #import "OCDDeskScene.h"
+#import "OCDDraggableObject.h"
+
+@interface OCDDeskScene() <OCDDraggableObjectDelegate>
+
+// Static objects
+@property (nonatomic, strong) SKSpriteNode *coffeeStain;
+@property (nonatomic, strong) SKSpriteNode *writingOnDesk;
+
+// Draggable objects
+@property (nonatomic, strong) OCDDraggableObject *paper;
+
+@end
 
 @implementation OCDDeskScene
 
@@ -16,9 +28,63 @@
     if (self)
     {
         self.backgroundColor = RGB(255, 255, 235);
+        
+        [self p_setupObjects];
     }
     
     return self;
+}
+
+- (void)p_setupObjects
+{
+    // Coffee stain
+    _coffeeStain = [SKSpriteNode spriteNodeWithImageNamed:@"desk-stain"];
+    _coffeeStain.position = ccp(self.size.width * 0.2, self.size.height * 0.7);
+    [self addChild:_coffeeStain];
+    
+    // Writing on desk
+    _writingOnDesk = [SKSpriteNode spriteNodeWithImageNamed:@"desk-text"];
+    _writingOnDesk.position = ccp(self.size.width * 0.9, self.size.height * 0.38);
+    [self addChild:_writingOnDesk];
+    
+    // Paper
+#warning - TODO: Update target position
+    _paper = [[OCDDraggableObject alloc] initWithRenderingNode:[SKSpriteNode spriteNodeWithImageNamed:@"desk-paper"] targetPosition:CGPointZero];
+    _paper.delegate = self;
+    _paper.position = ccp(self.size.width * 0.22, self.size.height * 0.52);
+    [self addChild:_paper];
+}
+
+#pragma mark - OCDDraggableDelegate methods
+- (void)touchStartedOnDraggableObject:(OCDDraggableObject *)object
+{
+    NSString *shadowFilename;
+    if ([object isEqual:self.paper])
+    {
+        shadowFilename = @"desk-paper-shadow";
+    }
+    UIImage *dragImage = [UIImage imageNamed:shadowFilename];
+    SKSpriteNode *renderingNode = (SKSpriteNode*)[object childNodeWithName:OCDDraggableObjectRenderingNodeName];
+    renderingNode.texture = [SKTexture textureWithImage:dragImage];
+    renderingNode.size = dragImage.size;
+}
+
+- (void)touchEndedOnDraggableObject:(OCDDraggableObject *)object
+{
+    NSString *normalFilename;
+    if ([object isEqual:self.paper])
+    {
+        normalFilename = @"desk-paper";
+    }
+    UIImage *normalImage = [UIImage imageNamed:normalFilename];
+    SKSpriteNode *renderingNode = (SKSpriteNode*)[object childNodeWithName:OCDDraggableObjectRenderingNodeName];
+    renderingNode.texture = [SKTexture textureWithImage:normalImage];
+    renderingNode.size = normalImage.size;
+}
+
+- (void)objectDidLockIntoPosition:(OCDDraggableObject *)object
+{
+    
 }
 
 @end
