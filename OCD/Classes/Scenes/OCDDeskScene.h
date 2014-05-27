4aaffd000942806368ae9ec0c05a3eb033654faa
Michael Gao <mgao.ucla@gmail.com>
2014-05-26 23:04:41 -0700

    Set up desk scene; added desk assets

---
+++ OCD/Classes/Scenes/OCDDeskScene.h
@@ -0,0 +1,13 @@
+//
+//  OCDDeskScene.h
+//  OCD
+//
+//  Created by Michael Gao on 5/26/14.
+//  Copyright (c) 2014 Chin and Cheeks. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface OCDDeskScene : SKComponentScene
+
+@end
---
+++ OCD/Classes/Scenes/OCDDeskScene.m
@@ -0,0 +1,24 @@
+//
+//  OCDDeskScene.m
+//  OCD
+//
+//  Created by Michael Gao on 5/26/14.
+//  Copyright (c) 2014 Chin and Cheeks. All rights reserved.
+//
+
+#import "OCDDeskScene.h"
+
+@implementation OCDDeskScene
+
+- (id)initWithSize:(CGSize)size
+{
+    self = [super initWithSize:size];
+    if (self)
+    {
+        self.backgroundColor = RGB(255, 255, 235);
+    }
+    
+    return self;
+}
+
+@end
